c4c709cf3ef12a85ffc63e6a8f614dcd4041bc22
Sedat-yalcin <36017667+Sedat-yalcin@users.noreply.github.com>
2020-08-07 19:22:56 +0200

    beforeUnityKit
    beforeUnityKit

---
+++ GrassRoadRace/Assets/GrassRoadRace/Script/TileManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+    public float zSpawn=0;
+    public float tileLenght = 30;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpwanTile(0);
+        SpwanTile(1);
+        SpwanTile(2);
+        SpwanTile(3);
+        SpwanTile(4);
+        SpwanTile(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpwanTile(int tileIndex)
+    {
+        Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        zSpawn += tileLenght;
+
+    }
+}
